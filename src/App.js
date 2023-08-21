3ef07589afc34a923565411bcb72a0d3da42dd3e
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2023-08-21 17:26:42 +0900

    start: 라우터 기본 구조 작성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
